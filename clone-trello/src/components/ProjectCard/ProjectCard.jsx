554bf15b91eeb2d781b76274f674255510f042e6
ClementHamonDev <clement.hamon35@gmail.com>
2024-09-19 11:26:14 +0200

    Création des 1er props

--- clone-trello/src/components/ProjectCard/ProjectCard.jsx
+++ clone-trello/src/components/ProjectCard/ProjectCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './ProjectCard.css';
 
-export default function ProjectCard () {
+export default function ProjectCard (props) {
   return (
     <div className="project-card">
-      <h3>Clone de Trello</h3>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
     </div>
   );
 };
