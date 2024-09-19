c3364d544c8f58cb80a7ac15eb7675a0c26c53fe
ClementHamonDev <clement.hamon35@gmail.com>
2024-09-19 11:20:35 +0200

    Création du 1er composant

--- clone-trello/src/App.js
+++ clone-trello/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import ProjectCard from './components/ProjectCard/ProjectCard';
 
 function App() {
   return (
@@ -7,15 +7,9 @@ function App() {
       <header className="App-header">
         <h1>Trello</h1>
       </header>
-      <main>
+      <main className="App-main">
         <h2>Bienvenue à cette formation</h2>
-        <p>
-          Cette formation est destinée à vous permettre de comprendre les
-          principes de base de React.
-        </p>
-        <p>
-          Nous allons voir comment recréer une application simple dans le style de Trello en React.
-        </p>
+        <ProjectCard />
       </main>
     </div>
   );
---
+++ clone-trello/src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ProjectCard.css';
+
+export default function ProjectCard () {
+  return (
+    <div className="project-card">
+      <h3>Clone de Trello</h3>
+    </div>
+  );
+};
