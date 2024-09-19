324fc29e2327f98cc2eacc9d62426f74a2de88af
ClementHamonDev <clement.hamon35@gmail.com>
2024-09-19 11:14:20 +0200

    init du projet

---
+++ clone-trello/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Trello</h1>
+      </header>
+      <main>
+        <h2>Bienvenue à cette formation</h2>
+        <p>
+          Cette formation est destinée à vous permettre de comprendre les
+          principes de base de React.
+        </p>
+        <p>
+          Nous allons voir comment recréer une application simple dans le style de Trello en React.
+        </p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
